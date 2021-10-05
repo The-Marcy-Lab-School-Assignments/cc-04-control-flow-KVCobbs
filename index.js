74e12968aa3ac75168d832c3cc8c43d25867379c
KVCobbs <kenyacobbs9001@gmail.com>
2021-10-05 14:58:16 +0000

    Finished first fizz buzz

--- index.js
+++ index.js
@@ -1,11 +1,24 @@
 //1
 function fizzBuzz(){
+  for(let i = 0; i <= 100; i++) 
+  {
+      
+   if(i % 15  == 0 )
+      console.log("FizzBuzz");
+      
+  else if (i % 3 == 0 ) console.log ('Fizz'); 
+ 
+  else if (i % 5 == 0 ) console.log ('Buzz'); 
   
+  else console.log(i);
+  }
 }
 
 //2
+/*
 function fizzBuzz(number){
   
 }
+*/
 
-
+fizzBuzz()
